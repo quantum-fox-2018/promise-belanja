5fce2c63f061b2f28e35098a32d94a2494279473
agungcaproex <agung.caproex@gmail.com>
2018-03-21 11:10:55 +0700

    nested promise

---
+++ index.js
@@ -0,0 +1,46 @@
+const beli = require('./beli')
+
+let obj = [{
+    item: 'Roti',
+    harga: 3000,
+    waktu: 800
+},{
+    item: 'Kopi',
+    harga: 2000,
+    waktu: 1000
+},{
+    item: 'Gula',
+    harga: 10000,
+    waktu: 800
+},{
+    item: 'Mie',
+    harga: 2500,
+    waktu: 1000
+},{
+    item: 'Batagor',
+    harga: 5000,
+    waktu: 500
+}]
+
+beli(20000, obj[0])
+.then((kembalian) => {
+    beli(kembalian, obj[1])
+    .then((kembalian) => {
+        beli(kembalian, obj[2])
+        .then((kembalian) => {
+            beli(kembalian, obj[3])
+            .then((kembalian) => {
+                beli(kembalian, obj[4])
+                .then((kembalian) => {
+                    return kembalian
+                })
+                .catch((reject) => {
+                    
+                })
+            })
+        })
+    })
+})
+.catch((reject) => {
+
+})
